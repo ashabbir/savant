4990d7d93803752b91bd307fba5a390ef01e69dc
Ahmed Shabbir <ahmed.shabbir@nyu.edu>
2025-11-15 22:50:14 -0500

    cli: add list/call subcommands using registrar discovery (TDD)

---
+++ spec/savant/cli/cli_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'savant CLI' do
+  it 'lists tools for the context service' do
+    cmd = %(ruby bin/savant list tools --service=context)
+    out = `#{cmd}`
+    expect($CHILD_STATUS.success?).to be(true)
+    expect(out).to include('fts/search')
+  end
+end
+
