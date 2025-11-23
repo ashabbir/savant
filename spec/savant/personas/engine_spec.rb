b20814ecc9140e6c18bd53e20f34da37e44096d4
Ahmed Shabbir <ahmed.shabbir@nyu.edu>
2025-11-23 10:56:35 -0500

    feat(personas): implement list/get tools, YAML catalog, and ops

--- spec/savant/personas/engine_spec.rb
+++ spec/savant/personas/engine_spec.rb
@@ -2,14 +2,22 @@
 
 require 'spec_helper'
 require_relative '../../../lib/savant/personas/tools'
+require_relative '../../../lib/savant/personas/engine'
 
 RSpec.describe Savant::Personas::Tools do
-  it 'builds registrar with example tool' do
-    engine = double('engine')
+  it 'exposes personas.list and personas.get tools' do
+    engine = Savant::Personas::Engine.new
     reg = described_class.build_registrar(engine)
-    specs = reg.specs
-    expect(specs.any? { |s| s[:name] == 'personas/hello' }).to be(true)
-    out = reg.call('personas/hello', { 'name' => 'dev' }, ctx: { engine: engine })
-    expect(out).to eq({ hello: 'dev' })
+    names = reg.specs.map { |s| s[:name] }
+    expect(names).to include('personas.list')
+    expect(names).to include('personas.get')
+
+    list = reg.call('personas.list', { 'filter' => 'savant' }, ctx: { engine: engine })
+    expect(list).to be_a(Hash)
+    expect(list[:personas] || list['personas']).to be_a(Array)
+
+    get = reg.call('personas.get', { 'name' => 'savant-engineer' }, ctx: { engine: engine })
+    expect((get[:name] || get['name'])).to eq('savant-engineer')
+    expect((get[:prompt_md] || get['prompt_md']).to_s).to include('Savant Engineer')
   end
 end
